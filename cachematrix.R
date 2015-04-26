487f9b772b1ef642354df43107f68519eb30a7e1
unknown <mpeesapati@112052787NB005.corp.logitech.com>
2015-04-26 00:05:27 -0700

    commit assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix creates a special vector that can cache its
+## inverse. Function cacheSolve returns the inverse of a matrix if already
+## cached. If not cached it calcutes the inverse and sets in cache 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## makeCacheMatrix function creates a special matrix that returns a list to
+## 1.Set the value of the matrix
+## 2.Get the value of the matrix
+## 3.Set the value of the inverse of a matrix
+## 4.Get the value of the inverse of a matrix
+
+makeCacheMatrix <- function(m=matrix()) {
+
+	i <- NULL
+	## set the value of the matrix
+	set <- function(y) {
+		m <<- y
+		i <<- NULL
+	}
+
+	## get the value of the matrix
+	get <- function() i
+
+	## set the value of the inverse of a matrix
+	setinv <- function(inverse) i <<- inverse
+
+	## get the value of the inverse of a matrix
+	getinv <- funtion() i
+
+	## return a list
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculates the inverse of special matrix returned
+## by makeCacheMatrix. If the inverse has already been calculated
+## (and the matrix has not changed), then the cachesolve will retrieve
+## the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m,...) {
+
+
+	## get inverse of matrix m from cache
+	i <- m$getinv()
+	
+	## if already calculated return inverse from cache
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)	
+	}
+	
+	## if not already calculated, calculate inverse
+	data <- m$get()
+	i <- solve(data, ...)
+	
+	## set the value of the inverse in cache
+	m$setinv(i)
+	
+	## return inverse of matrix m
+	i
 }
